87bc2bdcf79a4939aacc02bd106d69392c9d63e0
Antonio Costa <antonio_scosta@hotmail.com>
2023-03-13 23:59:57 -0300

    maps e botao pesquisar

--- app/src/views/HomeClienteView.vue
+++ app/src/views/HomeClienteView.vue
@@ -3,13 +3,18 @@
     <v-row class="text-center">
 		<v-col cols="12">
 			<h1 class="text-center font-weight-medium my-10 text-prymary" color="primary">Descubra profissionais perto de você</h1>
-			<v-text-field
-				filled
-				label="Buscar serviços"
-				prepend-inner-icon="mdi-magnify"
-				v-model="termo"
-				@keyup.enter="pesquisar"
-			></v-text-field>
+			<div class="d-flex">
+				<v-text-field
+					filled
+					label="Buscar serviços"
+					prepend-inner-icon="mdi-magnify"
+					v-model="termo"
+					@keyup.enter="pesquisar"
+				></v-text-field>
+				<v-btn class="d-flex ml-2 btn-azul mt-3" @click="pesquisar()">Pesquisar</v-btn>
+			</div>
+			
+			
 		</v-col>		
     </v-row>
     <v-row dense>
@@ -26,7 +31,7 @@
     </v-row>
 	<v-row>
 		<v-col cols="12">
-			<img src="@/assets/maps.png" />
+			<div class="mapouter"><div class="gmap_canvas"><iframe width="100%" height="510" id="gmap_canvas" src="https://maps.google.com/maps?q=brasil&t=&z=10&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://2yu.co">2yu</a><br></div></div>
 		</v-col>
 	</v-row>
   </v-container>
